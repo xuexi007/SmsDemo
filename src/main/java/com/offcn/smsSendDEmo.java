b83ebbd5edbdc95907d18a60b5113ad7d466ab8b
java0722 <test@qq.com>
2019-12-17 15:21:29 +0800

    测试github集成IdEA

---
+++ src/main/java/com/offcn/smsSendDEmo.java
@@ -0,0 +1,47 @@
+package com.offcn;
+
+import com.aliyuncs.CommonRequest;
+import com.aliyuncs.CommonResponse;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.exceptions.ServerException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+/*
+pom.xml
+<dependency>
+  <groupId>com.aliyun</groupId>
+  <artifactId>aliyun-java-sdk-core</artifactId>
+  <version>4.0.3</version>
+</dependency>
+*/
+public class smsSendDEmo {
+    public static void main(String[] args) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI4Ft6MN9A9zHJuncsxFyB", "DJVz4SkY2caqEBSWXnoUNcM7fByuFi");
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        CommonRequest request = new CommonRequest();
+        request.setMethod(MethodType.POST);
+        request.setDomain("dysmsapi.aliyuncs.com");
+        request.setVersion("2017-05-25");
+        request.setAction("SendSms");
+        request.putQueryParameter("RegionId", "cn-hangzhou");
+        //接收手机号
+        request.putQueryParameter("PhoneNumbers", "13521281544");
+        //签名
+        request.putQueryParameter("SignName", "优就业");
+        //模板编号
+        request.putQueryParameter("TemplateCode", "SMS_167532466");
+        //发送验证码值
+        request.putQueryParameter("TemplateParam", "{\"code\":\"88888\"}");
+        try {
+            CommonResponse response = client.getCommonResponse(request);
+            System.out.println(response.getData());
+        } catch (ServerException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
