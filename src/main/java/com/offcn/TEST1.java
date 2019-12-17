a38a8ca918107ba9a44db0b40a75d49c24ec2ecc
java0722 <test@qq.com>
2019-12-17 16:26:43 +0800

    老鸟新增test1\test3

--- src/main/java/com/offcn/TEST1.java
+++ src/main/java/com/offcn/TEST1.java
@@ -4,4 +4,18 @@ public class TEST1 {
     public static void main(String[] args) {
         System.out.println("测试代码");
     }
+
+    //老鸟新增方法test1
+    public void test1(){
+        boolean is=true;
+        if(is){
+            System.out.println("test1");
+        }
+    }
+
+    //老鸟新增方法test3
+    public void test3(){
+        System.out.println("test3");
+    }
+
 }
