a1f2e6fbb17d5bf1c4085163b9080baba3fe9f32
java0722 <test@qq.com>
2019-12-17 16:25:04 +0800

    小白修改 新增方法test1\test2

--- src/main/java/com/offcn/TEST1.java
+++ src/main/java/com/offcn/TEST1.java
@@ -4,4 +4,16 @@ public class TEST1 {
     public static void main(String[] args) {
         System.out.println("测试代码");
     }
+
+    //小白新增方法1
+    public void test1(){
+        for (int i = 0; i < 100; i++) {
+            System.out.println("i:"+i);
+        }
+    }
+
+    //小白新增方法2
+    public void test2(){
+        System.out.println("test2");
+    }
 }
